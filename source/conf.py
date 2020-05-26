7b6b823aa2763aa1efc01a2f60e200475c6a1638
Lorenzo Bellani <lorenzo.bellani@studenti.univr.it>
2020-05-26 12:08:58 +0200

    master

--- source/conf.py
+++ source/conf.py
@@ -17,9 +17,8 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Integration'
-copyright = '2020, Lorenzo Bellani'
-author = 'Lorenzo Bellani'
+project = 'Telenia Documentation'
+copyright = '2020, Telenia Software'
 
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
@@ -30,7 +29,10 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark']
+extensions = [
+    'recommonmark',
+    'sphinx_markdown_tables'
+    ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -46,7 +48,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
